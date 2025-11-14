1392e0ae86a2e802d95de5084d397eda44bd2df6
folajindayo <temidayofolajin@yahoo.com>
2025-11-14 04:36:40 +0100

    Implement ERC1155 standard with AccessControl and Pausable

--- contracts/JumpAchievements.sol
+++ contracts/JumpAchievements.sol
@@ -2,14 +2,42 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 
 /**
  * @title JumpAchievements
  * @dev ERC1155 token for Jump game achievement badges and collectibles
  */
-contract JumpAchievements is ERC1155 {
-    constructor() ERC1155("") {
-        // Initial setup
+contract JumpAchievements is ERC1155, AccessControl, Pausable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
+    
+    string public name = "Jump Achievements";
+    string public symbol = "JUMP-ACH";
+    
+    /**
+     * @dev Constructor sets up the ERC1155 with base URI
+     * Grants DEFAULT_ADMIN_ROLE, MINTER_ROLE, PAUSER_ROLE to deployer
+     */
+    constructor(string memory baseURI) ERC1155(baseURI) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(URI_SETTER_ROLE, msg.sender);
+    }
+    
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC1155, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
 
