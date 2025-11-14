11209fd70a7f53e88ade5e8073d4156564ff8c79
folajindayo <temidayofolajin@yahoo.com>
2025-11-14 04:31:15 +0100

    Create basic JumpToken ERC20 contract structure

---
+++ contracts/JumpToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title JumpToken
+ * @dev Basic ERC20 token structure for Jump game rewards
+ */
+contract JumpToken is ERC20 {
+    constructor() ERC20("Jump Token", "JUMP") {
+        // Initial setup
+    }
+}
+
